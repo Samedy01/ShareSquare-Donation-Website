c89f54f5116e757d0989ce71184ee7484008ec6a
Samedy01 <samedy.phin@student.cadt.edu.kh>
2023-05-23 18:32:59 +0700

    added fonts, images, campaign_profile view, css

---
+++ resources/views/donations/campaign_profile.blade.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/fontawesome.min.css" />  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" />  
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="campaign-profile donation px-14">
+        <div class="pb-12">
+            <div class="donation-title text-5xl pr-36">Building Decent School For Chhuk Village in Kompot</div>
+            <div class="h-8"></div>
+            <div class="donation-subtitle text-3xl">
+                <p class="donation-less-focus-text">
+                    Created by <span class="donation-more-focus-text">Ly Sovortey</span> • 7 May 2022
+                </p>
+            </div>
+        </div>
+
+        <div class="tmp bg-green-200 rounded-md clearfix">
+            <div class="float-left w-3/5 bg-yellow-300">
+                <img src="/img/donation/campaign_img.png" class="rounded-xl w-full" alt="">
+                <div class="p-8">
+                    Under Image Content
+                </div>
+            </div>
+
+
+
+            <div class="float-left w-2/5 bg-white pl-12">
+                <div class="border bg-white rounded-xl shadow px-3 py-5">
+                    {{-- info text --}}
+                    <p>
+                        <span class="primary-text-color text-4xl font-medium">$4456 </span>
+                        <span class="title-color text-2xl font-medium">raised of $20,000 goal</span>
+                    </p>
+                    {{-- progress slide bar --}}
+                    <div class="clearfix py-5">
+                        <div class="h-3 primary-bg-color w-1/4 float-left rounded-l-md"></div>
+                        <div class="h-3 bg-gray-300 w-3/4 float-left rounded-r-md"></div>
+                    </div>
+                    {{-- info text --}}
+                    <div>
+                        <p>
+                            <span class="dark-blue-grey">135 donations</span>
+                        </p>
+                    </div>
+                </div>
+
+                {{-- donate button --}}
+                <div class="py-6 border border-blue-400">
+                    <button class="w-full block primary-bg-color text-center py-6 rounded-xl">
+                        <span class="text-white font-bold text-2xl">Donate Now</span>
+                    </button>
+                </div>
+
+                {{-- options --}}
+                <div class="py-6 flex justify-between">
+                    @php
+                        $options = [
+                            ['text' => 'Share', 'icon' => 'fa fa-share'],
+                            ['text' => 'Care', 'icon' => 'fa fa-heart'],
+                            ['text' => 'Follow', 'icon' => 'fa fa-user-plus'],
+                        ];
+                    @endphp
+                    @foreach ($options as $option)
+                        <div class="item">
+                            <div class="py-2 px-5 shadow bg-white rounded-xl text-center">
+                                <i class="dark-blue-grey {{$option['icon']}} h-5"></i>
+                                <div class="h-1"></div>
+                                <p class="title-color text-xl font-medium">{{$option['text']}}</p>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+
+                {{-- top donors --}}
+                <div class="">
+                    <div class="title text-xl pb-3">Top Donors</div>
+                    <div class="">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="pb-12"></div>
+
+        {{-- tabs --}}
+        <div class="p-3 shadow text-center">
+            <div class="inline-block clearfix">
+                @php
+                    $tabs = ['Overview', 'Report', 'Comments', 'Donators'];
+                    $focus = 'more-focus';
+                @endphp
+
+                @foreach($tabs as $tab)
+                    @if (!$loop->first)
+                        @php
+                            $focus = 'less-focus';
+                        @endphp 
+                    @endif
+                    <div class="item {{$focus}} inline-block float-left py-3 px-8 border-r text-xl font-medium">{{$tab}}</div>
+                @endforeach
+
+            </div>
+        </div>
+
+        <div class="pb-12"></div>
+
+        <div class="donation-details">
+
+            @php
+                $items = [
+                    'Overview', 'Introduction', 'Challenge', 'Solution', 'Long-Term Impact', 'Contact Information'
+                ];   
+            @endphp
+
+            @foreach($items as $item)
+                <div class="item pb-16">
+                    <div class="title text-4xl pb-12">
+                        {{ $item }}
+                    </div>
+                    <div class="content">
+                        Our campaign "Building Decent School For Chhuk Village in Kompot" aims to provide a better learning environment for children in the Chhuk Village of Kompot. This village lacks access to a proper school building, leaving the children to study in overcrowded and unsafe classrooms. Our goal is to raise enough funds to construct a new school building that is safe, comfortable, and conducive to learning. By providing a decent school, we hope to improve the quality of education for the children in this village and give them a brighter future. We believe that every child deserves access to education and a chance to succeed, and with your support, we can make this a reality for the children of Chhuk Village.
+                    </div>
+                </div>
+            @endforeach
+
+            <button class="border primary-border-color rounded-xl p-6 block w-full">
+                <span class="primary-text-color text-center font-semibold text-2xl">Add New Subtitle</span>
+            </button>
+        </div>
+    </div>
+</body>
+</html>
