1a1f674d7125fae5da151cf5089a9757eafd83ec
Samedy01 <samedy.phin@student.cadt.edu.kh>
2023-06-26 23:13:33 +0700

    lang

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 class HomeController extends Controller
 {
     public function index(){
+        App::setLocale('kh');
         return view('dashboard');
     }
 }
